56be2e7e8fe41429ecabb2d572d083ba32123259
Blake Stevenson <BlakeStevenson@users.noreply.github.com>
2019-10-25 12:16:17 -0500

    Added additional statistics & changed r/funny to r/deepfriedmemes
    Created additional checks to see if a file is in a correct format, not
    downloading it at all if is not, replacing the previous approach of
    downloading everything then using an rm command to delete invalid
    files. Stats are displayed at the bottom of the console after running

--- index.js
+++ index.js
@@ -2,18 +2,20 @@ const RedditScraper = require("reddit-scraper");
 const exec = require('child_process').exec;
 const url = require('url');
 const fs = require('fs');
+var ProgressBar = require('ascii-progress');
 
 console.log("=====================");
 console.log("Reddit Meme Scraper");
 console.log("by Blake Stevenson");
 console.log("=====================");
+console.log("");
 
  (async () => {
- 	const directory = "YOUR_DIRECTORY_HERE";
- 	
+    const directory = "/var/www/api/meme/img";
+    
     const redditScraperOptions = {
-        AppId: "APP_ID_HERE",
-        AppSecret: "SECRET_HERE",
+        AppId: "vjngiZOerTtZxw",
+        AppSecret: "7XQ2yqcNj0DMDTd87GyZIsE88Ko",
     };
  
     const memes = {
@@ -23,12 +25,12 @@ console.log("=====================");
         SortType: "hot",
     };
     
-	const meme = {
+	/* const meme = {
         Pages: 100,
         Records: 25,
         SubReddit: "meme",
         SortType: "hot",
-    };
+    }; */
     
     const dankMemes = {
         Pages: 100,
@@ -37,12 +39,12 @@ console.log("=====================");
         SortType: "hot",
     };
     
-    const funny = {
+    const deepFriedMemes = {
         Pages: 100,
         Records: 25,
-        SubReddit: "funny",
+        SubReddit: "deepfriedmemes",
         SortType: "hot",
-    };
+    }; 
     
     const comedyCemetery = {
         Pages: 100,
@@ -63,40 +65,50 @@ console.log("=====================");
     	console.log("Configuration Loaded!");
     	var memesData = await redditScraper.scrapeData(memes);
     	console.log("Memes Subreddit Scraped!")
-    	var memeData = await redditScraper.scrapeData(meme);
-    	console.log("Meme Subreddit Scraped!");
+    	//var memeData = await redditScraper.scrapeData(meme);
+    	//console.log("Meme Subreddit Scraped!");
     	var dankMemesData = await redditScraper.scrapeData(dankMemes);
     	console.log("DankMemes Subreddit Scraped!");
-    	var funnyData = await redditScraper.scrapeData(funny);
-    	console.log("Funny Subreddit Scraped!");
+    	var deepFriedMemesData = await redditScraper.scrapeData(deepFriedMemes);
+    	console.log("DeepFriedMemes Subreddit Scraped!");
     	var comedyCemeteryData = await redditScraper.scrapeData(comedyCemetery);
     	console.log("ComedyCemetery Subreddit Scraped!");
     	var memeEconomyData = await redditScraper.scrapeData(memeEconomy);
     	console.log("MemeEconomy Subreddit Scraped!");
 
-	const scrapedData = [].concat.apply([], [memesData, memeData, dankMemesData, funnyData, comedyCemeteryData, memeEconomyData]);
-        console.log(scrapedData.length);
+	const scrapedData = [].concat.apply([], [memesData, /*memeData,*/ dankMemesData, deepFriedMemesData, comedyCemeteryData, memeEconomyData]);
 	var memeCount = 0;
+	var skipCount = 0;
+	var invalidCount = 0;
         for(i = 0; i < scrapedData.length; i++) {
         	var link = scrapedData[i].data.url;
-        	var file_name = url.parse(link).pathname.split('/').pop();    	
+        	var file_name = url.parse(link).pathname.split('/').pop();   
+			var ext = file_name.split('.').pop();
+			if(ext == "jpg" || ext == "jpg" || ext == "gif") {
         	try {
-  if(fs.existsSync(directory + "/" + file_name)) {
-    console.log("File already exists! Skipping...");
+ 			if(fs.existsSync(directory + "/" + file_name)) {
+    			//console.log("File already exists! Skipping...");
+    			skipCount++;	
   } else {
   	exec('wget -O ' + directory + "/" + file_name + ' ' + link);
-  	console.log("Saved " + file_name + "! (" + i + ")");
+  	// console.log("Saved " + file_name + "! (" + i + ")");
+  	
 	memeCount++; 
  }
 } catch(err) {
   console.error(err)
 }
+} else {
+	invalidCount++;
+}
   }
+console.log("");
 console.log("=========");
-console.log(memeCount + " memes successfully downloaded!");
-exec("rm -rf " + directory + "/*.gifv");
-exec('cd ' + directory + ' && find . -type f  ! -name "*.*"  -delete')
-console.log("Deleted .gifv and extensionless files.")
+console.log(memeCount + " memes successfully downloaded.");
+console.log(skipCount + " memes already downloaded.");
+console.log(invalidCount + " memes skipped because of an invalid format.")
+console.log("---------")
+console.log(scrapedData.length + " total memes fetched.")
     } catch (error) {
         console.error(error);
     }
